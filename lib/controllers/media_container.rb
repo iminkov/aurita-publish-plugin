4d5880c732c82d99e1efef6c020359cb79f4a0d7
Tobias Fuchs <twh.fuchs@gmail.com>
2010-01-18 02:34:15 +0100

    - Fixed Publish::Media_Container#set_position guards. - Using new Controller.use_model(model_klass) to delegate to model Wiki::Media_Container.

--- lib/controllers/media_container.rb
+++ lib/controllers/media_container.rb
@@ -8,8 +8,10 @@ module Publish
 
   class Media_Container_Controller < Aurita::Plugins::Wiki::Media_Container_Controller
 
-    guard_interface(:set_position) { 
-      mc = load_instance()
+    use_model Aurita::Plugins::Wiki::Media_Container
+
+    guard_interface(:set_position) { |controller|
+      mc = controller.load_instance()
       Aurita.user.may_edit_content?(mc.article)
     }
 
@@ -18,7 +20,6 @@ module Publish
       media_container = params[:part]
       GUI::Media_Container_Partial.new(media_container)
     end
-
 
     def set_position
       mc = load_instance()
